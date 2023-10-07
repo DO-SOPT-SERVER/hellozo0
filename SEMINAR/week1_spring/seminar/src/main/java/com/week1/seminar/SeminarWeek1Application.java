e7eb2065e98f98a0689b981eb876280a154f2557
hellozo0 <ahju0011@gmail.com>
2023-10-07 16:23:30 +0900

    Make new folder for seminar week1

---
+++ SEMINAR/week1_spring/seminar/src/main/java/com/week1/seminar/SeminarWeek1Application.java
@@ -0,0 +1,13 @@
+package com.week1.seminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarWeek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarWeek1Application.class, args);
+	}
+
+}
---
+++ SEMINAR/week1_spring/seminar/src/test/java/com/week1/seminar/SeminarWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week1.seminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarWeek1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
