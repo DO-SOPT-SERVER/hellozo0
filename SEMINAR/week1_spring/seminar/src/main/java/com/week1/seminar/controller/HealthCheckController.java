7b8c3a7cc402d8441d1ff94b4d80abcf73aab535
hellozo0 <ahju0011@gmail.com>
2023-10-07 17:18:31 +0900

    healthCheckController API 개발

--- SEMINAR/week1_spring/seminar/src/main/java/com/week1/seminar/controller/HealthCheckController.java
+++ SEMINAR/week1_spring/seminar/src/main/java/com/week1/seminar/controller/HealthCheckController.java
@@ -1,6 +1,8 @@
 package com.week1.seminar.controller;
 
 
+import com.week1.seminar.dto.HealthCheckResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +19,26 @@ public class HealthCheckController {
         Map<String, String> response = new HashMap<>();
         response.put("status", "OK");
         return response;
+    }
+    @GetMapping("/v2")
+    public ResponseEntity<String> healthCheckV2() {
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/v3")
+    public String healthCheckV3() {
+        return "OK";
+    }
+
+    @GetMapping("/v4")
+    public ResponseEntity<Map<String,String>> healthCheckV4() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "OK");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/v5")
+    public ResponseEntity<HealthCheckResponse> healthCheckV5() {
+        return ResponseEntity.ok(new HealthCheckResponse());
     }
 }
