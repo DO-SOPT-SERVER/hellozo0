901224ae27ae48c76b7cdb1b964131cc9b2db416
hellozo0 <ahju0011@gmail.com>
2023-10-07 17:18:41 +0900

    healthCheckController API 개발

---
+++ SEMINAR/week1_spring/seminar/src/main/java/com/week1/seminar/dto/HealthCheckResponse.java
@@ -0,0 +1,11 @@
+package com.week1.seminar.dto;
+
+public class HealthCheckResponse {
+
+    private static final String STATUS = "OK";
+    private String status;
+
+    public HealthCheckResponse() {
+        this.status = STATUS;
+    }
+}
