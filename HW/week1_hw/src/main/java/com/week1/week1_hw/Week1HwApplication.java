a504c019d332292d29c18cc935b57bfee47a44ab
hellozo0 <ahju0011@gmail.com>
2023-10-09 21:00:34 +0900

    ➕[ADD] : 1주차 과제 초기 세팅

---
+++ HW/week1_hw/src/main/java/com/week1/week1_hw/Week1HwApplication.java
@@ -0,0 +1,13 @@
+package com.week1.week1_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1HwApplication.class, args);
+	}
+
+}
