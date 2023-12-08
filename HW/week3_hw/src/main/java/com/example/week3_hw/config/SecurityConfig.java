9ce38736a5fd1e0b93a31a7520949d404be7c400
hellozo0 <ahju0011@gmail.com>
2023-12-08 23:58:26 +0900

    [FEAT] JWT설정 파일

--- HW/week3_hw/src/main/java/com/example/week3_hw/config/SecurityConfig.java
+++ HW/week3_hw/src/main/java/com/example/week3_hw/config/SecurityConfig.java
@@ -7,18 +7,46 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    private static final String[] AUTH_WHITELIST = {"/sign-up","/sign-in"}; //문제 포인트
+
+    public SecurityConfig() {
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequests) ->
-                authorizeRequests.anyRequest().permitAll())
+//        return http.csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests((authorizeRequests) ->
+//                authorizeRequests.anyRequest().permitAll())
+//                .build();
+        return http.csrf().disable()  //. 접근 불가
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(customJwtAuthenticationEntryPoint)
+                .accessDeniedHandler(customAccessDeniedHandler)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
@@ -34,4 +62,6 @@ public class SecurityConfig {
             }
         };
     }
+
+
 }
